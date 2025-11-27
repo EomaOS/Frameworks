9b82fdd2918ae768e38743cca54d4cfa8fbea194
aluapana135-design <aluapana135@gmail.com>
2025-11-27 11:59:33 -0300

    Atualizar o code_errors.c

--- code_err/code_errors.c
+++ code_err/code_errors.c
@@ -1,6 +1,9 @@
+/** copyright (C) EomaOS 2025-2026
+* code_err/code_errors.c */
 #include <cstdio.h>
-
-#define ERROR_SYSTEM0 "ERROR IN INSTALL PLEASE RECOVERY🤖"
+#include <stdio.h>
+#define ERROR_SYSTEM0 "ERROR IN INSTALL PLEASE RECOVERY🤖 \n"
 #define ERROR_NO_COMMAND "🤖⚠️
-NO COMMAND."
-#define ERROR_CAMERA "CAMERA_SENSOR_ERROR 0xUNKNOWN error in HAL .../hal/camera_sensor.hal"
+NO COMMAND. \n"
+#define ERROR_CAMERA "CAMERA_SENSOR_ERROR 0xUNKNOWN error in HAL .../hal/camera_sensor.hal \n"
+error_detected = 1;
