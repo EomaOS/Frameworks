bd9c20b27d1dbe504571b6168c17a84891515781
aluapana135-design <aluapana135@gmail.com>
2025-11-27 11:50:41 -0300

    Criar code_errors.c

---
+++ code_err/code_errors.c
@@ -0,0 +1,6 @@
+#include <cstdio.h>
+
+#define ERROR_SYSTEM0 "ERROR IN INSTALL PLEASE RECOVERY🤖"
+#define ERROR_NO_COMMAND "🤖⚠️
+NO COMMAND."
+#define ERROR_CAMERA "CAMERA_SENSOR_ERROR 0xUNKNOWN error in HAL .../hal/camera_sensor.hal"
